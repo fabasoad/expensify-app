ad7c639b2ef5146ff8d3ab0435cf62bec65d39b0
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-11-20 20:37:31 +0900

    Add setExpenses action

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -21,6 +21,8 @@ export default (state = expensesReducerDefaultState, action) => {
                     return e;
                 }
             });
+        case 'SET_EXPENSES':
+            return action.expenses;
         default:
             return state;
     }
