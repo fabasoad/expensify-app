4f2baca524c82e90f37ebea322c5d106f21cf371
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-11-22 19:18:04 +0900

    Styiling Login page

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,6 @@ const config = {
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
-console.log(config);
 
 firebase.initializeApp(config);
 firebase.analytics();
@@ -20,93 +19,3 @@ const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default };
-
-// database.ref('expenses').on('child_removed', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// database.ref('expenses').on('child_changed', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// database.ref('expenses').on('child_added', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// // database.ref('expenses').on('value', (snapshot) => {
-// //     const expenses = [];
-// //     snapshot.forEach((childSnapshot) => {
-// //         expenses.push({
-// //             id: childSnapshot.key,
-// //             ...childSnapshot.val()
-// //         });
-// //     });
-// //     console.log(expenses);
-// // });
-
-// // database.ref('expenses').push({
-// //     description: 'Hot Dog',
-// //     amount: 250,
-// //     createdAt: 3000,
-// //     note: 'It was tasty'
-// // });
-
-// // database.ref('expenses').push({
-// //     description: 'Coffee',
-// //     amount: 100,
-// //     createdAt: 4000,
-// //     note: 'Espresso'
-// // });
-
-// // database.ref('expenses').push({
-// //     description: 'Milk',
-// //     amount: 200,
-// //     createdAt: 5000,
-// //     note: 'Without lactose'
-// // });
-
-// // database.ref('notes').push({
-// //     body: 'React, Angular, Python',
-// //     title: 'Course topics'
-// // });
-
-// // database.ref('location/city')
-// //     .once('value')
-// //     .then((snapshot) => {
-// //         const val = snapshot.val();
-// //         console.log(val);
-// //     }).catch((e) => {
-// //         console.log('Failed to fetch the data', e);
-// //     });
-
-// // database.ref().set({
-// //     name: 'Yevhen Fabizhevskyi',
-// //     age: 31,
-// //     stressLevel: 6,
-// //     job: {
-// //         title: 'Software Engineer',
-// //         company: 'Google'
-// //     },
-// //     location: {
-// //         city: 'Tokyo',
-// //         country: 'Japan'
-// //     }
-// // }).then(() => {
-// //     console.log('Data is saved');
-// // }).catch((e) => {
-// //     console.log('Failed to save the data.', e);
-// // });
-
-// // database.ref().update({
-// //     stressLevel: 9,
-// //     'job/company': 'Amazon',
-// //     'location/city': 'Kobe'
-// // });
-
-// // database.ref('isSingle')
-// //     .remove()
-// //     .then(() => {
-// //         console.log('Data was removed');
-// //     }).catch((e) => {
-// //         console.log('Data was not removed', e);
-// //     });
