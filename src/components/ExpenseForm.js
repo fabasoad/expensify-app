5961ace4097ec7cfb16b76bb5144f01e3739adcb
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-11-23 23:52:29 +0900

    Remove playground folder

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -48,7 +48,7 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: undefined }));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 100,
+                amount: parseFloat(this.state.amount) * 100,
                 note: this.state.note,
                 createdAt: this.state.createdAt.valueOf()
             });
