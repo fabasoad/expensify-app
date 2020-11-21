690eb03ca57eeda09b7de17a1f1d424c4b02dc24
fabasoad <fabasoad@gmail.com>
2020-11-21 09:43:26 +0900

    Upgrade dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,11 +3,11 @@ const path = require('path');
 const common = require('./webpack.common.js')();
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-        publicPath: '/'
-    }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: '/'
+  }
 });
