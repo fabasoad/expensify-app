b69d0b3f3be4b3bd6220e98c58dd4554c09859c9
fabasoad <fabasoad@gmail.com>
2020-12-28 17:33:11 +0900

    Fix CI

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });
@@ -11,47 +11,52 @@ if (process.env.NODE_ENV === 'test') {
 
 module.exports = () => {
   return {
-  entry: [
-    '@babel/polyfill',
-    path.resolve(__dirname, 'src', 'app.js')
-  ],
-  module: {
-    rules: [{
-    test: /\.js$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/
-    }, {
-    test: /\.s?css$/,
-    use: [{
-      loader: 'style-loader'
-    }, {
-      loader: 'css-loader',
-      options: {
-      sourceMap: true
-      }
-    }, {
-      loader: 'sass-loader',
-      options: {
-      sourceMap: true
-      }
-    }]
-    }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-    'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-    'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-    'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-    'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-    'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-    'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-    'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-    'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
-    })
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public', 'dist'),
-  }
+    entry: [
+      '@babel/polyfill',
+      path.resolve(__dirname, 'src', 'app.jsx')
+    ],
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }, {
+        test: /\.s?css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }]
+      }]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+      })
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public', 'dist'),
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
   }
 };
